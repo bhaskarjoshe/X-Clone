44caa84df4060ae7285a97da637b123beebb1406
Bhaskar Chandra Joshi <bhaskarjoshe@gmail.com>
2025-02-13 15:09:04 +0530

    8. Celery Implementation

--- CoreX/CoreX/settings.py
+++ CoreX/CoreX/settings.py
@@ -198,7 +198,7 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 
 # Celery Configuration
-CELERY_BROKER_URL = "redis://localhost:6379/0"
+CELERY_BROKER_URL = os.getenv('REDIS_URL', "redis://localhost:6379/0")
 CELERY_ACCEPT_CONTENT = ["json"]
 CELERY_TASK_SERIALIZER = "json"
 
