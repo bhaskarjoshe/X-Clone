f8d13ed71af5fad3c2272608d4e46dc2a967388c
Bhaskar <bhaskarjoshe@gmail.com>
2025-05-31 22:48:39 +0530

    cloudinry setup to get media files

--- CoreX/CoreX/settings.py
+++ CoreX/CoreX/settings.py
@@ -49,6 +49,8 @@ INSTALLED_APPS = [
     "app_tweets",
     "app_comments",
     "app_follow",
+    "cloudinary",
+    "cloudinary_storage",
 ]
 
 
@@ -142,14 +144,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static"),
-]
-# STATIC_ROOT = BASE_DIR / "static/"
+# STATICFILES_DIRS = [os.path.join(BASE_DIR / "static"),]
+STATIC_ROOT = BASE_DIR / "static/"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 MEDIA_URL = "/media/"
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+
+
+CLOUDINARY_STORAGE = {
+    "CLOUD_NAME": os.getenv("CLOUDINARY_CLOUD_NAME"),
+    "API_KEY": os.getenv("CLOUDINARY_API_KEY"),
+    "API_SECRET": os.getenv("CLOUDINARY_API_SECRET"),
+}
+
+DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
